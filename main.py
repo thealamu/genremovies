bdf1ca06e0fd30e18854d296d755a8cb5d2d624d
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-26 19:27:08 +0100

    Define the endpoints

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+SHOWINGS_ENDPOINT_FMT = "https://data.tmsapi.com/v1.1/movies/showings?startDate={0}&zip={1}&api_key={2}"
+AIRINGS_ENDPOINT_FMT = "http://data.tmsapi.com/v1.1/movies/airings?startDateTime={0}&lineupId={1}&api_key={2}"
+
+
+def getShowings(api_secret, start_date, zip_code):
+    pass
+
+
+def getAirings(api_secret, start_datetime, line_up_id):
+    pass
+
+
+def getData():
+    pass
